689a3785edca18bdad306b24ded576bbe0dc984a
James Dunn <jamesldunnjr@gmail.com>
2013-03-27 08:02:43 -0400

    adding a new model

---
+++ Matchismo/Model/PlayingCard.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCard.h
+//  Matchismo
+//
+//  Created by James Dunn on 3/27/13.
+//  Copyright (c) 2013 James Dunn. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface PlayingCard : Card
+
+@end
---
+++ Matchismo/Model/PlayingCard.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCard.m
+//  Matchismo
+//
+//  Created by James Dunn on 3/27/13.
+//  Copyright (c) 2013 James Dunn. All rights reserved.
+//
+
+#import "PlayingCard.h"
+
+@implementation PlayingCard
+
+@end
