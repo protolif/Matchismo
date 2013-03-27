f78614b509dba5130a44b9d0a64f2d36d010dbda
James Dunn <jamesldunnjr@gmail.com>
2013-03-27 05:06:14 -0400

    adding a new deck model

---
+++ Matchismo/Model/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  Matchismo
+//
+//  Created by James Dunn on 3/27/13.
+//  Copyright (c) 2013 James Dunn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Deck : NSObject
+
+@end
---
+++ Matchismo/Model/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  Matchismo
+//
+//  Created by James Dunn on 3/27/13.
+//  Copyright (c) 2013 James Dunn. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
