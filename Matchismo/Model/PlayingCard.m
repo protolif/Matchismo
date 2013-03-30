b60a25015771a762ac454444ebcaf74104a6677b
James Dunn <jamesldunnjr@gmail.com>
2013-03-30 10:36:26 -0400

    PlayingCard rankStrings[0] should return ?

--- Matchismo/Model/PlayingCard.m
+++ Matchismo/Model/PlayingCard.m
@@ -17,7 +17,7 @@
   + (NSArray *)rankStrings {
     static NSArray *rankStrings = nil;// C static is like Ruby constant
     // Here, there be dragons. But I'll venture a guess:
-    if (!rankStrings) rankStrings = @[@"A", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"J", @"Q", @"K"];
+    if (!rankStrings) rankStrings = @[@"?", @"A", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"J", @"Q", @"K"];
     return rankStrings;
   }
 
