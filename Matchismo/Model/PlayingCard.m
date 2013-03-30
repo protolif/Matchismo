641a6336107877845eef18aecf0a91da1d651e6c
James Dunn <jamesldunnjr@gmail.com>
2013-03-30 10:03:54 -0400

    rankStrings: returns an array of playing card rank strings, from Ace to King.

--- Matchismo/Model/PlayingCard.m
+++ Matchismo/Model/PlayingCard.m
@@ -16,7 +16,8 @@
 
   + (NSArray *)rankStrings {
     static NSArray *rankStrings = nil;// C static is like Ruby constant
-    // Here, there be dragons.
+    // Here, there be dragons. But I'll venture a guess:
+    if (!rankStrings) rankStrings = @[@"A", @"2", @"3", @"4", @"5", @"6", @"7", @"8", @"9", @"10", @"J", @"Q", @"K"];
     return rankStrings;
   }
 
