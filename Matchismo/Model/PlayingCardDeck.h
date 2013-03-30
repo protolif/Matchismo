f80ab3d3136d9f9eadb616dddbe164d9faaea955
James Dunn <jamesldunnjr@gmail.com>
2013-03-30 11:46:11 -0400

    new model: PlayingCardDeck

---
+++ Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by James Dunn on 3/30/13.
+//  Copyright (c) 2013 James Dunn. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
---
+++ Matchismo/Model/PlayingCardDeck.m
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.m
+//  Matchismo
+//
+//  Created by James Dunn on 3/30/13.
+//  Copyright (c) 2013 James Dunn. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+
+@implementation PlayingCardDeck
+
+@end
