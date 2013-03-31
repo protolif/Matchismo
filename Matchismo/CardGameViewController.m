82ca22f62c6963238f0c0b1e8f40fce55935cc10
James Dunn <jamesldunnjr@gmail.com>
2013-03-30 20:15:46 -0400

    adding homework 1 instructions to the comments

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -5,6 +5,11 @@
 //  Created by James Dunn on 3/23/13.
 //  Copyright (c) 2013 James Dunn. All rights reserved.
 //
+//  Homework 1: Make each flip draw a new card
+//  (instead of showing A♣ all the time).
+//  Just add a deck property to your controller.
+//  Then draw a random card from it,
+//  with each flip to face up.
 
 #import "CardGameViewController.h"
 
