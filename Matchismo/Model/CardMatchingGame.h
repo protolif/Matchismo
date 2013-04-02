20420624cdaecba41a3be9a34b27a5913cf11b3b
James Dunn <jamesldunnjr@gmail.com>
2013-04-02 14:26:21 -0400

    new model: card matching game. game logic will live here.

---
+++ Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by James Dunn on 4/2/13.
+//  Copyright (c) 2013 James Dunn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matchismo/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by James Dunn on 4/2/13.
+//  Copyright (c) 2013 James Dunn. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
