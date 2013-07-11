0f647b61ed179f39cc381fa2d8c7cd24cc354a2f
James Dunn <jamesldunnjr@gmail.com>
2013-07-11 15:43:32 -0400

    Added a tab bar with custom icons and a HiScoresViewContorller. Also figured out the provisioning profile *

---
+++ Matchismo/Controller/HiScoresViewController.h
@@ -0,0 +1,13 @@
+//
+//  HiScoresViewController.h
+//  Matchismo
+//
+//  Created by proto on 7/11/13.
+//  Copyright (c) 2013 James Dunn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HiScoresViewController : UIViewController
+
+@end
---
+++ Matchismo/Controller/HiScoresViewController.m
@@ -0,0 +1,38 @@
+//
+//  HiScoresViewController.m
+//  Matchismo
+//
+//  Created by proto on 7/11/13.
+//  Copyright (c) 2013 James Dunn. All rights reserved.
+//
+
+#import "HiScoresViewController.h"
+
+@interface HiScoresViewController ()
+
+@end
+
+@implementation HiScoresViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
